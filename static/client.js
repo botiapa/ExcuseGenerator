574a224e287d76db9000dbb92e0feeaf077d5398
Killswitch <szentesi.botond@gmail.com>
2020-06-04 19:59:54 +0200

    Small fixes

--- static/client.js
+++ static/client.js
@@ -69,10 +69,16 @@ function register()
 // HELPER METHODS
 function generate() {
     var xhttp = new XMLHttpRequest();
+    var output = document.getElementById("output");
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-        document.getElementById("output").innerHTML = xhttp.responseText;
+            // If we got the same excuse, refresh
+            if(output.innerHTML == xhttp.responseText) {
+                generate();
+            }
+            else {
+                output.innerHTML = xhttp.responseText;
+            }
         }
     };
     if(lang == 0) // English 
